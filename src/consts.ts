2b8e943d0e4ef8202d033c1a6f53e56460ce18a2
Vlad Kudelko <vlad1kudelko@gmail.com>
2025-10-21 20:09:19 +0800

    ui

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+export const SITE_NAME        = 'Craft Homelab';
+export const SITE_TITLE       = 'Craft Homelab - Полезные гайды и инструкции для разработчиков';
+export const SITE_DESCRIPTION = 'Подробные статьи и пошаговые руководства по Django, Python, Telegram-ботам и современным технологиям. Всё, что нужно для эффективной разработки и развертывания проектов.';
+
+export const LINK_CONTACTS    = '/contacts/'
+export const TELEGRAM_CHANNEL = 'https://t.me/+URA3cZoIm603MjUy'
