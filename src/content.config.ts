b27a51bcc7e5b4468bc8b614ac2155ea866f6834
vir <you@example.com>
2025-11-25 11:20:48 +0800

    tags

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,13 +3,13 @@ import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
     loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
-    schema: ({ image }) =>
-        z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string(),
         pubDate: z.coerce.date(),
         heroImage: image().optional(),
         github: z.string().optional(),
+        tags: z.string().optional(),
     }),
 });
 
--- src/content/posts/2025/10/12-timelinize-timelinize.md
+++ src/content/posts/2025/10/12-timelinize-timelinize.md
@@ -4,7 +4,7 @@ description: "Timelinize позволяет импортировать и объ
 heroImage: "../../../../assets/imgs/2025/10/12-timelinize-timelinize.webp"
 pubDate: "2025-10-12"
 github: "https://github.com/timelinize/timelinize"
-top: true
+tags: "top"
 ---
 
 <!-- [35.16, 86] > [0, 80] -->
