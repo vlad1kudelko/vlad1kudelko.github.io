50ce105801f626ae2cbcbd6866e0cc5637f142e2
vir <you@example.com>
2025-12-04 16:55:40 +0800

    links

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,7 @@ export const SITE_NAME        = 'Craft Homelab';
 export const SITE_TITLE       = 'Craft Homelab - Полезные гайды и инструкции для разработчиков';
 export const SITE_DESCRIPTION = 'Подробные статьи и пошаговые руководства по Django, Python, Telegram-ботам и современным технологиям. Всё, что нужно для эффективной разработки и развертывания проектов.';
 
+export const LINK_DOCS        = '/docs/';
 export const LINK_CONTACTS    = '/contacts/';
 export const TELEGRAM_CHANNEL = 'https://t.me/+URA3cZoIm603MjUy';
 
