3bd5d66989ba118c7cf33fe60c3a07db2474561c
vir <you@example.com>
2025-12-04 14:38:49 +0800

    -

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,10 @@ export const SITE_NAME        = 'Craft Homelab';
 export const SITE_TITLE       = 'Craft Homelab - Полезные гайды и инструкции для разработчиков';
 export const SITE_DESCRIPTION = 'Подробные статьи и пошаговые руководства по Django, Python, Telegram-ботам и современным технологиям. Всё, что нужно для эффективной разработки и развертывания проектов.';
 
-export const LINK_CONTACTS    = '/contacts/'
-export const TELEGRAM_CHANNEL = 'https://t.me/+URA3cZoIm603MjUy'
+export const LINK_CONTACTS    = '/contacts/';
+export const TELEGRAM_CHANNEL = 'https://t.me/+URA3cZoIm603MjUy';
 
-export const AUTHOR_NAME      = 'Vlad Kudelko'
+export const AUTHOR_NAME      = 'Vlad Kudelko';
+
+export const HOME_H1          = 'Полезные гайды и инструкции для разработчиков';
+export const DOCS_H1          = 'Документация к популярным библиотекам';
