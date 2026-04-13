52c2d65073542b43a7401bcb8ab1deed13a76c71
Vlad Kudelko <vlad1kudelko@gmail.com>
2026-04-13 14:06:01 +0800

    верстка

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import pagefind from 'astro-pagefind';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    site: 'https://vlad1kudelko.github.io',
-    integrations: [mdx(), sitemap(), pagefind()],
+  site: "https://vlad1kudelko.github.io",
+  integrations: [mdx(), sitemap()],
 
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
