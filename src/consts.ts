96413da89ad67056d173f098bcd775f33e486436
Vlad Kudelko <vlad1kudelko@gmail.com>
2026-09-10 17:06:00 +0000

    h1

--- src/consts.ts
+++ src/consts.ts
@@ -11,9 +11,9 @@ export const DOCS_OG_IMAGE = "/res/all/thumb.webp";
 export const DOCS_H1 = "Документация к популярным библиотекам";
 
 export const AI_TITLE = "Нейросети по API: рейтинг сервисов доступа";
-export const AI_DESCR = "Сравнение сервисов доступа к нейросетям по API: способы регистрации, оплата криптой и российскими картами, генерация текста, картинок, видео и музыки.";
+export const AI_DESCR = "Сравнение агрегаторов нейросетей: способы регистрации, оплата криптой и российскими картами, генерация текста, картинок, видео и музыки.";
 export const AI_OG_IMAGE = "/res/all/thumb.webp";
-export const AI_H1 = "Нейросети по API: где взять доступ";
+export const AI_H1 = "Нейросети по API: сравнение сервисов";
 
 export const LINK_DOCS = "/docs/";
 export const LINK_AI = "/ai/";
